f367cc2d785feb50e54818dcf5ecdfeb65a6de61
damzymichael <105460672+damzymichael@users.noreply.github.com>
2024-05-02 09:37:53 +0100

    db uri removed

--- src/util/env.ts
+++ src/util/env.ts
@@ -2,7 +2,6 @@ import {cleanEnv, email, port, str} from 'envalid';
 
 export default cleanEnv(process.env, {
   PORT: port(),
-  DB_URI: str(),
   JWT_SECRET: str(),
   ADMIN_EMAIL: email(),
   ADMIN_PASSWORD: str(),
